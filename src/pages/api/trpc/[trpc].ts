96196c1c95c7df238699d7d48a7d31fe1b2460c0
Alexey Immoreev <lexx92@mail.ru>
2022-06-07 15:31:19 +0400

    next.config.js CORS headers

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,4 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { NextApiHandler } from "next";
 import { appRouter } from "../../../router";
 
 const trpcHandler = trpcNext.createNextApiHandler({
@@ -7,16 +6,4 @@ const trpcHandler = trpcNext.createNextApiHandler({
   createContext: (opts) => opts,
 });
 
-const handler: NextApiHandler = (req, res) => {
-  if (req.method === "OPTIONS") {
-    const origin = req.headers.origin || "*";
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Max-Age", 86400);
-    return res.send("OK");
-  }
-  return trpcHandler(req, res);
-};
-
-export default handler;
+export default trpcHandler;
